394bc1d8d408a2a2851d5f34349c337a66a49dae
sroik <vasili.kazhanouski@gmail.com>
2020-05-14 18:05:15 +0300

    public viewbox (#1)

--- Source/svg/SVGNodeLayout.swift
+++ Source/svg/SVGNodeLayout.swift
@@ -38,10 +38,9 @@ class SVGSize {
 
 }
 
-protocol NodeLayout {
-
+public protocol NodeLayout {
+    var viewBox: Rect? { get }
     func computeSize(parent: Size) -> Size
-
     func layout(node: Node, in size: Size)
 }
 
